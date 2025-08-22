f0e4d22c075e58752deaffa90326f0fa12d42ffe
portfiend <109661617+portfiend@users.noreply.github.com>
2025-08-21 21:46:11 -0400

    move job preference buttons into correct namespace

--- Content.Client/_MORBIT/Lobby/UI/CharacterEditor/JobPreferenceButtons.xaml.cs
+++ Content.Client/_MORBIT/Lobby/UI/CharacterEditor/JobPreferenceButtons.xaml.cs
@@ -11,7 +11,7 @@ using Robust.Shared.Prototypes;
 using Robust.Shared.Utility;
 
 #pragma warning disable IDE1006
-namespace Content.Client._MORBIT.Lobby.UI;
+namespace Content.Client._MORBIT.Lobby.UI.CharacterEditor;
 #pragma warning restore IDE1006
 
 [GenerateTypedNameReferences]
